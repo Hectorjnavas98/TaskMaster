6ff0a66abef1797285ce15dd2fc27052252a9547
Raul Garre <rgarre@uoc.edu>
2024-03-23 13:12:31 +0100

    Correcciones formulario

---
+++ js/newboard.js
@@ -0,0 +1,10 @@
+let newBoardForm = document.getElementById("newBoardForm");
+
+newBoardForm.addEventListener("submit", (e) => {
+    //if(!isValid){
+    //    e.preventDefault();    //stop form from submitting
+    //}
+    // handle submit
+    window.open("./board.html");
+  });
+  
