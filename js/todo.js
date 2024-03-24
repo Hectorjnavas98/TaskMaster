cf97ffc657b9a895761eba43cab74e1ecfcaa8f4
Raul Garre <rgarre@uoc.edu>
2024-03-24 09:41:48 +0100

    Modales editar y borrar tareas

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,11 @@
 const form = document.getElementById("todo-form");
 const input = document.getElementById("todo-input");
 const todoLane = document.getElementById("todo-lane");
+const deleteModal = document.getElementById("delete-modal");
+const editModal = document.getElementById("edit-modal");
+
+let currentTask = null;
+let editText = null;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -8,10 +13,16 @@ form.addEventListener("submit", (e) => {
 
   if (!value) return;
 
-  const newTask = document.createElement("p");
-  newTask.classList.add("task");
+  const newTask = document.createElement("div");
+  const newText = document.createElement("p");
+  const buttonAdd = document.createElement("button");
+  const buttonDelete = document.createElement("button");
+  const spanAdd = document.createElement("span");
+  const spanDelete = document.createElement("span");
+
+  newTask.classList.add("task", "bg-white", "border", "rounded", "p-2", "m-1");
   newTask.setAttribute("draggable", "true");
-  newTask.innerText = value;
+  //newTask.innerText = value;
 
   newTask.addEventListener("dragstart", () => {
     newTask.classList.add("is-dragging");
@@ -21,7 +32,69 @@ form.addEventListener("submit", (e) => {
     newTask.classList.remove("is-dragging");
   });
 
+  newText.innerText = value;
+  newTask.append(newText);
+
+  //Button edit
+  buttonAdd.setAttribute("type","button");
+  buttonAdd.classList.add("btn", "float-end");  
+  buttonAdd.setAttribute("data-bs-toggle","modal");
+  buttonAdd.setAttribute("data-bs-target","#edit-modal");
+  spanAdd.classList.add("bi-pencil");
+  buttonAdd.append(spanAdd);
+
+  //Button delete
+  buttonDelete.setAttribute("type","button");
+  buttonDelete.classList.add("btn", "float-end");
+  buttonDelete.setAttribute("data-bs-toggle","modal");
+  buttonDelete.setAttribute("data-bs-target","#delete-modal");
+  spanDelete.classList.add("bi-trash");
+  buttonDelete.append(spanDelete);
+
+  newTask.append(buttonAdd);
+  newTask.append(buttonDelete);
+
   todoLane.appendChild(newTask);
 
   input.value = "";
 });
+
+editModal.addEventListener('show.bs.modal', function (event) {
+  let button = event.relatedTarget;
+  currentTask = button.parentNode;
+  console.log(currentTask)
+  let textTask = currentTask.children[0];
+  let modalBodyInput = editModal.querySelector('.modal-body input');
+  modalBodyInput.value = textTask.innerText;
+  console.log(textTask.innerText);
+})
+
+editModal.addEventListener('hide.bs.modal', function (event) {
+  let button = event.relatedTarget;
+  let modalBodyInput = editModal.querySelector('.modal-body input');
+  editText = modalBodyInput.value;
+})
+
+deleteModal.addEventListener('show.bs.modal', function (event) {
+  let button = event.relatedTarget;
+  currentTask = button.parentNode;
+  console.log(currentTask);
+})
+
+function changeTask() {
+  let taskText = currentTask.children[0];
+  if ( editText !== taskText.innerText ) {    
+    taskText.innerText = editText;
+  }
+}
+
+
+function deleteTask() {
+  if ( currentTask !== null ) {
+    currentTask.remove();
+  }
+}
+
+function cancelModal() {
+  currentTask = null;
+}
